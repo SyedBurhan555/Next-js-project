81f5c65434f416bd681bd5a65f142502c1b0727a
Syed Burhan <syedburhanali555@gmail.com>
2023-08-08 17:38:27 +0500

    feat/make-mongoose-db-connection

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    removeConsole: false,
+  },
+};
 module.exports = nextConfig;
